74e22806c86ac2a66a0b45bd75e49079d00e5ed1
Joseph <2handheld@gmail.com>
2024-07-02 05:10:59 +0100

    extract string in layouts and update version catalogs for implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,12 +50,9 @@ dependencies {
 
     implementation(libs.bundles.default)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test:rules:1.4.0")
+    androidTestImplementation(libs.bundles.androidTestImplementation)
 
+    testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.room:room-testing:2.6.0")
 
     ksp("androidx.room:room-compiler:2.6.0")
